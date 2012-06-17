bd92aa00ae9c11db384dd71161aa7c8b282de843
Simon Knight <simon.knight@gmail.com>
2012-06-17 18:04:15 +0930

    overlay accessors working

--- anm.py
+++ anm.py
@@ -1,33 +1,59 @@
 import networkx as nx
 from collections import namedtuple
 
-class DeviceNotFoundException(Exception):
-       def __init__(self, message):
-        Exception.__init__(self, message)
+class AutoNetkitException(Exception):
+    pass
 
-class overlay_node(namedtuple('node', "anm, overlay_graph, node")):
+class IntegrityException(AutoNetkitException):
+    def __init__(self, Errors):
+        self.Errors = Errors
+
+    def __str__(self):
+        return "Device %s not found in physical graph" % self.Errors
+
+class DeviceNotFoundException(AutoNetkitException):
+    def __init__(self, message, Errors):
+        Exception.__init__(self, message)
+        self.Errors = Errors
+
+    def __str__(self):
+        return "Unable to find %s" % self.Errors
+
+class overlay_node(namedtuple('node', "anm, overlay_id, node_id")):
     """API to access overlay graph node in network"""
     __slots = ()
 
     @property
     def _graph(self):
         """Return graph the node belongs to"""
-        return self.anm._overlays[self.overlay_graph]
+        return self.anm._overlays[self.overlay_id]
+
+    @property
+    def _phy_node(self):
+# refer back to the physical node, to access attributes such as name
+        return overlay_node(self.anm, "phy", self.node_id)
 
     def __repr__(self):
         #return "Overlay for %s in %s" % (self.node.fqdn, self.graph)
 #TODO: label should come from node in physical graph
-        return self.anm.overlay.phy.device(self.node).label
+        #return self.anm.overlay.phy.device(self.node).label
+        try:
+            return self._phy_node.label
+        except IntegrityException:
+# Node not in physical graph
+            try:
+                return self._graph.node[self.node_id]['label']
+            except KeyError:
+                return self.node_id # No label set for this node, return node id
 
     def __getattr__(self, key):
         """Returns node property
         This is useful for accesing attributes passed through from graphml"""
 #TODO: make this log to debug on a miss, ie if key not found: do a try/except for KeyError for this
         try:
-            return self._graph[self.node].get(key)
-            pass
+            return self._graph[self.node_id].get(key)
         except KeyError:
-            raise DeviceNotFoundException
+            raise IntegrityException(self.node_id)
 
     def __setattr__(self, key, val):
         """Sets node property
@@ -47,15 +73,17 @@ class overlay_graph(namedtuple('overlay_graph', "anm, overlay_name")):
         return self.anm._overlays[self.overlay_name]
 
     def __iter__(self):
-        return iter(overlay_node(self.anm, self.overlay_graph, node)
+        return iter(overlay_node(self.anm, self.overlay_name, node)
                 for node in self._graph)
 
     def __getattr__(self, key):
+        print "ACCESS NODE IN GRAPH"
         """Access node in overlay graph"""
+        print "trying to access key %s in overlay %s" % (key, self.overlay_name)
         if key in self._graph:
             return overlay_node(self.anm, self.overlay_name, key)
         else:
-            raise DeviceNotFoundException("Unable to find %s" % key)
+            raise DeviceNotFoundException("Unable to find ", key)
 
     def device(self, key):
         """To access programatically"""
@@ -79,11 +107,12 @@ class overlay_accessor(namedtuple('overlay_accessor', "anm")):
         """Access overlay graph"""
         return overlay_graph(self.anm, key)
 
-    #    """Set overlay graph
-    #    TODO: do we want to restrict this? Ie import explicit function?
-    #    """
-    #    #self.node.network._graphs[self.graph].node[self.node][key] = val
-    #    self.anm._g_overlays[key] = val
+    def __setattr__(self, key, val):
+        """Set overlay graph
+        TODO: do we want to restrict this? Ie import explicit function?
+        """
+        #self.node.network._graphs[self.graph].node[self.node][key] = val
+        self.anm._overlays[key] = val
 
 class AbstractNetworkModel(object):
     
@@ -119,6 +148,12 @@ def load_graphml(filename):
 def f_phy(G_in):
     return G_in
 
+
+"""TODO: allow graphs to be frozen for integrity, 
+eg load input, freeze, 
+and once done with overlays freeze them before nidb
+"""
+
 anm = AbstractNetworkModel()
 G_in = load_graphml("multias.graphml")
 
@@ -134,3 +169,9 @@ anm.add_overlay("ip")
 anm.add_overlay("igp")
 anm.add_overlay("bgp")
 print anm.overlay
+
+# call platform compiler to build NIDB
+
+# pass to renderer
+
+
