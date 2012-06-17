b7c920ed1ed6674af33567d624849bc7d12f1fde
Simon Knight <simon.knight@gmail.com>
2012-06-17 18:24:15 +0930

    before named tuples - > proper classes

--- anm.py
+++ anm.py
@@ -23,6 +23,9 @@ class overlay_node(namedtuple('node', "anm, overlay_id, node_id")):
     """API to access overlay graph node in network"""
     __slots = ()
 
+#TODO: allow access back up to overlays from this
+# eg self.ip.property self.bgp.property etc
+
     @property
     def _graph(self):
         """Return graph the node belongs to"""
@@ -76,6 +79,9 @@ class overlay_graph(namedtuple('overlay_graph', "anm, overlay_name")):
         return iter(overlay_node(self.anm, self.overlay_name, node)
                 for node in self._graph)
 
+    def nodes(self):
+        return self.__iter__()
+
     def __getattr__(self, key):
         print "ACCESS NODE IN GRAPH"
         """Access node in overlay graph"""
@@ -111,7 +117,6 @@ class overlay_accessor(namedtuple('overlay_accessor', "anm")):
         """Set overlay graph
         TODO: do we want to restrict this? Ie import explicit function?
         """
-        #self.node.network._graphs[self.graph].node[self.node][key] = val
         self.anm._overlays[key] = val
 
 class AbstractNetworkModel(object):
@@ -119,6 +124,10 @@ class AbstractNetworkModel(object):
     def __init__(self):
         self._overlays = {}
         self.add_overlay("phy")
+
+    @property
+    def _phy(self):
+        return overlay_graph(self, "phy")
 
     def add_overlay(self, name, directed=False, multi_edge=False):
         """Adds overlay graph of name name"""
@@ -138,6 +147,26 @@ class AbstractNetworkModel(object):
     def overlay(self):
         return overlay_accessor(self)
 
+    def devices(self, **kwargs):
+        nodes = [n for n in self._phy]
+        print "nodes are", list(nodes)
+        nodes = self._phy.nodes() # All nodes in graph
+        print "nodes are", list(nodes)
+
+        # need to allow filter_func to access these args
+        def filter_func(n):
+            return all(self.graph.node[n].get(k) == v for k,v in
+                            kwargs.items())
+
+        return (n for n in nodes if filter_func(n))
+
+
+    def group_by(self, nodes, **kwargs):
+        """Groups nodes by argument, eg by asn, return as a dict... or as 
+        itertools? can this be easily made into a dict?"""
+        pass
+
+
 def load_graphml(filename):
     graph = nx.read_graphml(filename)
 # apply defaults
@@ -147,6 +176,9 @@ def load_graphml(filename):
 
 def f_phy(G_in):
     return G_in
+
+
+# probably want to create a graph from input with switches expanded to direct connections
 
 
 """TODO: allow graphs to be frozen for integrity, 
@@ -160,6 +192,7 @@ G_in = load_graphml("multias.graphml")
 anm.add_overlay("input")
 anm.overlay.input = G_in
 print anm.overlay.input
+print list(anm.devices())
 
 for device in anm.overlay.input:
     print device
