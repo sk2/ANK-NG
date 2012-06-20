9c72a6b6d023f53a01a0cd4f96727827c004cd79
Simon Knight <simon.knight@gmail.com>
2012-06-20 18:00:35 +0930

    tidy console script

--- console_script.py
+++ console_script.py
@@ -9,30 +9,21 @@ input_graph = ank.load_graphml("example.graphml")
 
 G_in = anm.add_overlay("input", input_graph)
 
-#G_phy created automatically by ank
-G_phy = anm.overlay.phy
+G_phy = anm.overlay.phy #G_phy created automatically by ank
 
 # build physical graph
-#TODO: add helper function to copy across label, device_type, x, y, etc
 G_phy.add_nodes_from(G_in, retain=['label', 'device_type', 'asn'])
 G_phy.add_edges_from([edge for edge in G_in.edges() if edge.type == "physical"])
 
-#TODO: need way to select nodes, eg sw1->r3
 G_phy.add_edge(G_phy.node("r1"), G_phy.node("r4"))
 
 G_graphics = anm.add_overlay("graphics") # plotting data
 G_graphics.add_nodes_from(G_in, retain=['x', 'y', 'device_type'])
 
-#TODO: add graphics interpolated for collision domain nodes
-
 G_ip = anm.add_overlay("ip")
 G_ip.add_nodes_from(G_in)
 G_ip.add_edges_from(G_in.edges(type="physical"))
 
-#print list(G_in.nodes())
-#print list(G_in.nodes(device_type="switch"))
-
-#explode_edges = ank.explode(G_ip, [n for n in G_ip if n.phy.device_type == "switch"])
 switch_nodes = [n for n in G_ip if n.phy.device_type == "switch"]
 ank.aggregate_nodes(G_ip, switch_nodes)
 #TODO: add function to update edge properties: can overload node update?
@@ -46,12 +37,6 @@ split_created_nodes = list(ank.split(G_ip, edges_to_split))
 for node in split_created_nodes:
     node.overlay.graphics.x = ank.neigh_average(G_ip, node, "x", G_graphics)
     node.overlay.graphics.y = ank.neigh_average(G_ip, node, "y", G_graphics)
-
-""" Averaging non numeric property
-for node in G_phy:
-    print ank.neigh_average(G_phy, node, "device_type", G_phy)
-
-"""
 
 #TODO: add sanity checks like only routers can cross ASes: can't have an eBGP server
 
@@ -70,17 +55,10 @@ for node in G_ip.nodes("collision_domain"):
     if not node.is_switch:
         node.label = "cd_%s" % cd_id # switches keep their names
 
-
 ank.allocate_ips(G_ip)
-#print G_ip.data.asn_blocks
-
-#G_ip.dump()
 ank.save(G_ip)
-#ank.plot(G_ip, edge_label_attribute="ip_address")
-
 
 G_bgp = anm.add_overlay("bgp", directed = True)
-
 G_bgp.add_nodes_from([d for d in G_in if d.is_router], color = 'red')
 
 # eBGP
@@ -89,41 +67,19 @@ G_bgp.add_edges_from(ebgp_edges, type = 'ebgp')
 
 # now iBGP
 for asn, devices in G_in.groupby("asn").items():
-    #print "iBGP for asn", asn
-
-    asn_subgraph = G_in.subgraph(devices, name="asn%s" % asn)
-
     routers = [d for d in devices if d.device_type == "router"]
     ibgp_edges = [ (s, t) for s in routers for t in routers if s!=t]
     G_bgp.add_edges_from(ibgp_edges, type = 'ibgp')
 
-# mark nodes according to properties
-#print "high degree nodes", [d for d in G_in if d.degree() > 2]
-
-#for n in G_bgp:
-    #print n, "has edges", list(n.edges())
-    #print "in edges", list(ank.in_edges(G_bgp, n))
-
 ebgp_nodes = [d for d in G_bgp if any(edge.type == 'ebgp' for edge in d.edges())]
-#print "ebgp nodes are", ebgp_nodes
 G_bgp.update(ebgp_nodes, ebgp=True)
-#print [d.ebgp for d in G_bgp]
 
-G_bgp.update([d for d in G_bgp if d.ebgp], ram = 64)
-
-#print "big ram", list(G_bgp.filter(ram=64))
-#G_phy.add_edge(G_phy.node("r3"), G_phy.node("r2"), speed=500)
-    
-#G_bgp.dump()
-#ank.plot(G_bgp, edge_label_attribute="type")
-#plot(G_phy)
 ank.save(G_bgp)
 ank.save(G_phy)
 
 #TODO: set fqdn property
 
 nidb = NIDB() 
-
 #TODO: build this on a platform by platform basis
 nidb.add_nodes_from(G_phy, retain=['label'])
 for node in nidb:
@@ -132,7 +88,6 @@ for node in nidb:
     node.graphics.y = graphics_node.y
     node.graphics.device_type = graphics_node.device_type
 
-#TODO: provide abstraction to work on a list/dict easily to store/retrieve from nidb
 # build IP
 for node in nidb:
     if node in G_ip:
@@ -147,13 +102,12 @@ for node in nidb:
         bgp_node = G_bgp.node(node)
         data = []
         asn = G_phy.node(node).asn
-        asn_sn_blocks = G_ip.data.asn_blocks[asn]
-        node.bgp.asn_sn_blocks = asn_sn_blocks
+        node.bgp.asn_sn_blocks = G_ip.data.asn_blocks[asn]
         for session in G_bgp.edges(bgp_node):
-            session_data = {}
-            session_data['type'] = session.type
-            session_data['peer'] = session.dst
-            data.append(session_data)
+            data.append({
+                    'type': session.type,
+                    'peer': session.dst,
+            })
         node.bgp.session = data
 
         #print node.bgp['session']
@@ -163,11 +117,6 @@ for node in nidb:
     node.render.template = "templates/test.mako"
     node.render.dst_folder = "rendered"
     node.render.dst_file = "%s.conf" % node.label
-
-#ank.save(nidb)
-#print nidb.dump()
-
-#TODO: perform a transform on NIDB, replacing all dicts with objects to make easy iteration
 
 #TODO: don't need to transform, just need to pass a view of the nidb which does the wrapping: iterates through returned data, recursively, and wraps accordingly. ie pass the data to return through a recursive formatter which wraps
 ank_render.render(nidb)
