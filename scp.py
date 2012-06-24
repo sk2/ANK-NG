50468ba60e57180ed11c34bf4d573f431367364c
Simon Knight <simon.knight@gmail.com>
2012-06-24 17:45:14 +0930

    pickling of nidb

--- scp.py
+++
@@ -1,9 +0,0 @@
-import paramiko
-ssh = paramiko.SSHClient()
-ssh.set_missing_host_key_policy(
-    paramiko.AutoAddPolicy())
-ssh.connect('trc1.trc.adelaide.edu.au', username='sknight')
-ftp = ssh.open_sftp()
-ftp.put('nren.graphml', 'remotefile.py')
-ftp.close()
-
