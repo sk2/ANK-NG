e82f6deba09de6bd6d08da0fe85acba0637fb606
Simon Knight <simon.knight@gmail.com>
2012-06-24 21:00:36 +0930

    more diffing

--- ank_diff.py
+++ ank_diff.py
@@ -2,6 +2,7 @@ import networkx as nx
 import glob
 import os
 import pprint
+from collections import defaultdict
 
 def diff_history(directory):
     glob_dir = os.path.join(directory, "*.pickle.tar.gz")
@@ -13,6 +14,38 @@ def diff_history(directory):
         graphB = nx.read_gpickle(fileB)
         compare(graphA, graphB)
 
+def element_diff(elemA, elemB):
+    try:
+        if len(elemA) > 1 and len(elemB) > 1:
+            list_diff(elemA, elemB)
+    except TypeError:
+        pass
+    if elemA != elemB:
+        return { 'before': elemA, 'after': elemB, }
+
+def list_diff(listA, listB):
+    listA = sorted(listA)
+    listB = sorted(listB)
+    elements = zip(listA, listB)
+    list_changed = []
+    for (elemA, elemB) in elements:
+        try:
+            elemA.keys() # see if both are dicts
+            elemB.keys()
+            elem_changed = dict_diff(elemA, elemB) # are dicts, compare as a dict
+            if elem_changed:
+                list_changed.append(elem_changed)
+        except AttributeError:
+            try:
+                if len(elemA) > 1 and len(elemB) > 1:
+                    return list_diff(elemA, elemB)
+            except AttributeError:
+                return element_diff(elemA, elemB)
+
+    if list_changed:
+        return list_changed
+
+
 
 def dict_diff(dictA, dictB):
     """Calls self recursively to see if any changes
@@ -20,39 +53,78 @@ def dict_diff(dictA, dictB):
     If no keys in self, returns None"""
     #TODO: if no keys then return items???
     #print "comparing", dictA, dictB
-    keysA = set(dictA.keys())
-    keysB = set(dictB.keys())
-    if keysA != keysB:
-        print "keys added:", ", ".join(sorted(keysB - keysA))
-        print "keys removed:", ", ".join(sorted(keysA - keysB))
-    
+    diff = defaultdict(dict)
+    try:
+        keysA = set(dictA.keys())
+        keysB = set(dictB.keys())
+    except AttributeError:
+# if list, compare list items
+        modified = element_diff(dictA, dictB)
+        if modified:
+            print modified
+        return
+
+
+#TODO: change commonKeys to common_keys
+
+    commonKeys = keysA & keysB
+    keys_modified = {}
+    for key in commonKeys:
+        subDictA = dictA[key]
+        subDictB = dictB[key]
+        changed = dict_diff(subDictA, subDictB)
+        if changed:
+            keys_modified[key] = changed
+
+    keys_added = keysB - keysA
+    if keys_added:
+        diff['added'] = keys_added
+    keys_removed = keysA - keysB
+    if keys_removed:
+        diff['removed'] = keys_removed
+    if keys_modified:
+        diff['modified'] = keys_modified
+
+    if diff:
+        #return dict(diff)
+        pass
 
 def compare(graphA, graphB):
     diff = {}
     nodesA = set(graphA.nodes())
     nodesB = set(graphB.nodes())
     commonNodes = nodesA & nodesB
-
-    diff['nodes'] = {
-            'added': nodesB - nodesA,
-            'removed': nodesA - nodesB,
-            }
+    diff = defaultdict(dict)
+    #diff['nodes'] = {
+            #'added': nodesB - nodesA,
+            #'removed': nodesA - nodesB,
+            #}
 
     for node in commonNodes:
         dictA = graphA.node[node]
         dictB = graphB.node[node]
-        dict_diff(dictA, dictB)
+        node_diff = dict_diff(dictA, dictB)
+        print "changed nodes"
+        pprint.pprint(node_diff)
 
     edgesA = set(graphA.edges())
     edgesB = set(graphB.edges())
     commonEdges = edgesA & edgesB
-    diff['edges'] = {
-            'added': edgesB - edgesA,
-            'removed': edgesA - edgesB,
-            }
+    added_edges = edgesB - edgesA
+    if added_edges:
+        diff['edges']['added'] = added_edges
+    removed_edges = edgesA - edgesB
+    if removed_edges:
+        diff['edges']['removed'] = removed_edges
+
+    for (src, dst) in commonEdges:
+        dictA = graphA[src][dst]
+        dictB = graphB[src][dst]
+        dict_diff(dictA, dictB)
 
 
-    pprint.pprint(diff)
+    #pprint.pprint(diff)
+
 
     
 """
