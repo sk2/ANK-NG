29a2331c402000c85f8d6cacce2d57c2055b3f73
Simon Knight <simon.knight@gmail.com>
2012-08-29 14:46:20 +0930

    before public commit

--- autonetkit/console_script.py
+++ autonetkit/console_script.py
@@ -148,14 +148,12 @@ def build_ospf(anm):
     G_ospf.remove_edges_from(non_same_asn_edges)
 
 def build_network(input_filename):
-
     anm = AbstractNetworkModel()
-
     input_graph = ank.load_graphml(input_filename)
 
     G_in = anm.add_overlay("input", input_graph)
-    ank.set_node_default(G_in, G_in.nodes(), platform="cisco")
-    ank.set_node_default(G_in, G_in.nodes(), host="demo")
+    ank.set_node_default(G_in, G_in.nodes(), platform="netkit")
+    ank.set_node_default(G_in, G_in.nodes(), host="nectar1")
     if len(ank.unique_attr(G_in, "asn")) > 1:
         # Multiple ASNs set, use label format device.asn 
         anm.set_node_label(".as",  ['label', 'asn'])
